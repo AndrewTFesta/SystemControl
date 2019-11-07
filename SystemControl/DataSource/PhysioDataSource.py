Scaling and filtering applied to udp data stream from obci Fixed errors in datatransformer and tfclassifier arising due to changes made in datasource

SystemControl/DataSource/PhysioDataSource.py
@@ -101,7 +101,10 @@ class PhysioDataSource(DataSource):
         data = cleaned_edf.get_data()
         np_data = np.transpose(data)
         sample_list = [
-            SampleEntry(idx=sample_idx, time=idx_to_time(sample_idx, self.sample_freq), data=each_sample)
+            SampleEntry(
+                idx=sample_idx, time=idx_to_time(sample_idx, self.sample_freq),
+                data=each_sample
+            )
             for sample_idx, each_sample in enumerate(np_data)
         ]
 

SystemControl/DataTransformer.py
@@ -123,101 +123,9 @@ def img_id(data_entry: np.ndarray):
     return entry_hash
 
 
-def generate_csp_dataset(data_source: DataSource, subject: str):
-    mne.set_log_level('CRITICAL')  # DEBUG, INFO, WARNING, ERROR, or CRITICAL
-    # # Set parameters and read data
-    # avoid classification of evoked responses by using epochs that start 1s after cue onset.
-    tmin, tmax = -1., 4.
-    event_id = dict(hands=2, feet=3)
-    subject = 1  # todo check for each subject
-    runs = [6, 10, 14]  # motor imagery: hands vs feet
-
-    raw_fnames = eegbci.load_data(subject, runs)
-    raw = concatenate_raws([read_raw_edf(f, preload=True) for f in raw_fnames])
-
-    # # strip channel names of "." characters
-    raw.rename_channels(lambda x: x.strip('.'))
-    # raw.pick_channels(['C3', 'Cz', 'C4'])  # fails when reducing number of channels
-
-    # # Apply band-pass filter
-    raw.filter(7., 30., fir_design='firwin', skip_by_annotation='edge')
-    events, _ = events_from_annotations(raw, event_id=dict(T1=2, T2=3))
-    picks = pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False, exclude='bads')
-
-    # Read epochs (train will be done only between 1 and 2s)
-    # Testing will be done with a running classifier
-    epochs = Epochs(raw, events, event_id, tmin, tmax, proj=True, picks=picks, baseline=None, preload=True)
-    # epochs_train = epochs.copy().crop(tmin=1., tmax=2.)
-    epochs_train = epochs.copy().crop(tmin=1., tmax=2.)
-    labels = epochs.events[:, -1] - 2
-
-    # # Define a monte-carlo cross-validation generator (reduce variance):
-    scores = []
-    epochs_data = epochs.get_data()
-    epochs_data_train = epochs_train.get_data()
-    cv = ShuffleSplit(10, test_size=0.2, random_state=42)
-    cv_split = cv.split(epochs_data_train)
-
-    # # Assemble a classifier
-    lda = LinearDiscriminantAnalysis()
-    csp = CSP(n_components=4, reg=None, log=True, norm_trace=False)
-
-    # Use scikit-learn Pipeline with cross_val_score function
-    clf = Pipeline([('CSP', csp), ('LDA', lda)])
-    scores = cross_val_score(clf, epochs_data_train, labels, cv=cv, n_jobs=1)
-
-    # Printing the results
-    class_balance = np.mean(labels == labels[0])
-    class_balance = max(class_balance, 1. - class_balance)
-    print("Classification accuracy: %f / Chance level: %f" % (np.mean(scores), class_balance))
-
-    # plot CSP patterns estimated on full data for visualization
-    csp.fit_transform(epochs_data, labels)
-
-    layout = read_layout('EEG1005')
-    csp.plot_patterns(epochs.info, layout=layout, ch_type='eeg', units='Patterns (AU)', size=1.5)
-
-    sfreq = raw.info['sfreq']
-    w_length = int(sfreq * 0.5)  # running classifier: window length
-    w_step = int(sfreq * 0.1)  # running classifier: window step size
-    w_start = np.arange(0, epochs_data.shape[2] - w_length, w_step)
-
-    scores_windows = []
-
-    for train_idx, test_idx in cv_split:
-        y_train, y_test = labels[train_idx], labels[test_idx]
-
-        X_train = csp.fit_transform(epochs_data_train[train_idx], y_train)
-        X_test = csp.transform(epochs_data_train[test_idx])
-
-        # fit classifier
-        lda.fit(X_train, y_train)
-
-        # running classifier: test classifier on sliding window
-        score_this_window = []
-        for sigs in w_start:
-            X_test = csp.transform(epochs_data[test_idx][:, :, sigs:(sigs + w_length)])
-            score_this_window.append(lda.score(X_test, y_test))
-        scores_windows.append(score_this_window)
-
-    # Plot scores over time
-    w_times = (w_start + w_length / 2.) / sfreq + epochs.tmin
-
-    plt.figure()
-    plt.plot(w_times, np.mean(scores_windows, 0), label='Score')
-    plt.axvline(0, linestyle='--', color='k', label='Onset')
-    plt.axhline(0.5, linestyle='-', color='k', label='Chance')
-    plt.xlabel('time (s)')
-    plt.ylabel('classification accuracy')
-    plt.title('Classification score over time')
-    plt.legend(loc='lower right')
-    plt.show()
-    return
-
-
 def generate_heatmap_dataset(
         data_source: DataSource, subject: str, start_padding: float, end_padding: float, spacing: int,
-        interpolation: Interpolation, cmap: CMAP, timing_resolution: int, verbose: bool = True, save_dir: str = None
+        interpolation: Interpolation, cmap: CMAP, timing_resolution: int, verbose: bool = True
 ):
     if verbose:
         print('------------------------------------------')
@@ -267,21 +175,19 @@ def generate_heatmap_dataset(
     if verbose:
         print(f'Time to build heatmaps: {e_time - s_time:0.4f} seconds')
 
+    dataset_dir = os.path.join(
+        DATA_DIR, 'heatmaps', data_source.name,
+        f'spad_{int(start_padding * timing_resolution)}', f'epad_{int(end_padding * timing_resolution)}',
+        f'{interpolation.name}', f'{subject}'
+    )
     s_time = time.time()
     for heatmap_event, heatmap_list in heatmap_dict.items():
-        if save_dir:
-            base_dir = save_dir
-        else:
-            base_dir = os.path.join(
-                DATA_DIR, 'heatmaps', data_source.name,
-                f'spad_{int(start_padding * timing_resolution)}', f'epad_{int(end_padding * timing_resolution)}',
-                f'{interpolation.name}', f'{subject}', f'{heatmap_event}'
-            )
+        event_dir = os.path.join(dataset_dir, f'{heatmap_event}')
 
-        if not os.path.isdir(base_dir):
-            os.makedirs(base_dir)
+        if not os.path.isdir(event_dir):
+            os.makedirs(event_dir)
         for heatmap_idx, each_heatmap in enumerate(heatmap_list):
-            out_fname = os.path.join(base_dir, f'{img_id(each_heatmap)}.png')
+            out_fname = os.path.join(event_dir, f'{img_id(each_heatmap)}.png')
             if os.path.isfile(out_fname):
                 os.remove(out_fname)
             # opencv faster than pillow: 2.63 it/s PIL vs 3.48 it/s CV2
@@ -293,50 +199,46 @@ def generate_heatmap_dataset(
         print('------------------------------------------')
         print(f'Generating dataset completed')
         print('------------------------------------------')
-    return
+    return dataset_dir
 
 
 def main():
-    data_source = PhysioDataSource()
-    # data_source = RecordedDataSource()
+    # data_source = PhysioDataSource()
+    data_source = RecordedDataSource()
 
     timing_resolution = 100
     start_padding = int(0.1 * timing_resolution) / timing_resolution
     end_padding_range = [
         int(each_epad * timing_resolution) / timing_resolution
-        for each_epad in [0.1, 0.2, 0.3, 0.4, 0.5]
+        # for each_epad in [0.1, 0.2, 0.3, 0.4, 0.5]
+        for each_epad in [0.5]
     ]
     spacing = 100
     cmap = CMAP.rocket_r
     verbose = False
 
-    total_count = len(data_source.subject_names)
+    total_count = len(data_source.subject_names[:2])
     total_count *= len(Interpolation.__members__)
     total_count *= len(end_padding_range)
 
     pbar = tqdm(total=total_count, desc='Generating heatmap dataset')
     timing_dict = {}
-    for each_subject in data_source.subject_names:
+    for each_subject in data_source.subject_names[:2]:
         data_source.set_subject(each_subject)
         timing_dict[each_subject] = {}
         for each_interpolation in Interpolation:
             timing_dict[each_subject][each_interpolation.name] = {}
             for each_end_padding in end_padding_range:
-                save_dir = os.path.join(
-                    DATA_DIR, 'heatmaps', data_source.name,
-                    f'spad_{int(start_padding * timing_resolution)}',
-                    f'epad_{int(each_end_padding * timing_resolution)}',
-                    f'{each_interpolation.name}', f'{each_subject}'
-                )
                 s_time = time.time()
-                generate_heatmap_dataset(
+                heatmap_dir = generate_heatmap_dataset(
                     data_source=data_source, subject=each_subject, interpolation=each_interpolation,
                     end_padding=each_end_padding, start_padding=start_padding, spacing=spacing,
-                    cmap=cmap, timing_resolution=timing_resolution, verbose=verbose, save_dir=save_dir
+                    cmap=cmap, timing_resolution=timing_resolution, verbose=verbose
                 )
                 e_time = time.time()
                 d_time = e_time - s_time
-                num_images = len(find_files_by_type('png', save_dir))
+                save_dir = os.path.join
+                num_images = len(find_files_by_type('png', heatmap_dir))
                 timing_dict[each_subject][each_interpolation.name][each_end_padding] = {
                     'time': d_time, 'num_images': num_images
                 }